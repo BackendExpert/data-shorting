12e6953e91cdd1115af25e5b70c50a5abc00cd8f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 20:23:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/DataShorting/DataShorting.tsx
+++ src/components/DataShorting/DataShorting.tsx
@@ -5,11 +5,19 @@ const DataShorting = () => {
   const [sortData, SetSortData] = useState("none")
 
   const MethodSort = {
-
+    none: {method: (a, b) => null},
+    atoz: {method: undefined},
+    ztoa: {method: (a, b) => (a > b ? -1 : 1)},
   };
-  
+
   return (
-    <div>DataShorting</div>
+    <div>
+      <select defaultValue={'DEFAULT'} onChange={(e) => SetSortData(e.target.value)}>
+          <option value="DEFAULT">None</option>
+          <option value="atoz">Ascending</option>
+          <option value="ztoa">Descending</option>
+      </select>
+    </div>
   )
 }
 
