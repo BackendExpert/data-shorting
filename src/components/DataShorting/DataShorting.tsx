cc2c56a93d1b03d7bbca4f9d798050950a0c682e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 20:16:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/DataShorting/DataShorting.tsx
+++ src/components/DataShorting/DataShorting.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import Datalist from "./DataList"
 
 const DataShorting = () => {
+  const [sortData, SetSortData] = useState("none")
+
+  const MethodSort = {
+
+  };
+  
   return (
     <div>DataShorting</div>
   )
