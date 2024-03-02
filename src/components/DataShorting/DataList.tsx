694524e24cf2735ef434fb8dabaf7ee29c54c132
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 19:57:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ src/components/DataShorting/DataList.tsx
@@ -0,0 +1,9 @@
+const Datalist = [
+    {name: "BMW", img: ""},
+    {name: "Audi", img: ""},
+    {name: "Suzuki", img: ""},
+    {name: "Yamaha", img: ""},
+    {name: "Lambogini", img: ""} 
+]
+
+export default Datalist
