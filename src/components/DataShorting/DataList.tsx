1d8fc2c4e66d0d3a0478a2e0e4cff6ed3f1d2010
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 20:13:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/DataShorting/DataList.tsx
+++ src/components/DataShorting/DataList.tsx
@@ -1,9 +1,9 @@
 const Datalist = [
-    {name: "BMW", img: ""},
-    {name: "Audi", img: ""},
-    {name: "Suzuki", img: ""},
-    {name: "Yamaha", img: ""},
-    {name: "Lambogini", img: ""} 
+    {name: "BMW", img: "https://wallpapercave.com/wp/wp2707503.jpg"},
+    {name: "Audi", img: "https://wallpapercave.com/wp/wp2707504.jpg"},
+    {name: "Suzuki", img: "https://wallpapercave.com/wp/wp2707507.jpg"},
+    {name: "Yamaha", img: "https://wallpapercave.com/wp/wp2707508.jpg"},
+    {name: "Lambogini", img: "https://wallpapercave.com/wp/wc1754678.jpg"} 
 ]
 
 export default Datalist
